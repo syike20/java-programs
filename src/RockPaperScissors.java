b9bea3472479b9b8acb18048aed5fae77fc0e2e0
syike20 <dashrathrao28@gmail.com>
2026-06-25 23:24:56 +0530

    Added Rock Paper Scissors Game

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -2,39 +2,63 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class RockPaperScissors {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        boolean restart = true ;
-        while (restart) {
-            String[] options  = {"ROCKS","PAPER","SCISSORS"};
-            int Opponent_choice = random.nextInt(options.length);
-            String randomOption = options[Opponent_choice];
-            String choice  ;
-            System.out.print("Enter your choice(ROCK,PAPER,SCISSORS): ");
-            choice= scanner.nextLine().toUpperCase();
+        //Welcome Message
+        System.out.println("----------------------------------");
+        System.out.println("Let's Play ROCK PAPER SCISSORS !!!");
+        System.out.println("----------------------------------");
 
-            if(choice.equals(randomOption))
-            {
+        //Game Logic
+        boolean Continue = true ;
+
+        while (Continue)
+        {
+            //Take player choice
+            System.out.print("Enter your choice (rock,paper,scissors): ");
+            String PlayerChoice = scanner.nextLine().toUpperCase();
+
+            //Generate Computer's Choice
+            String[] Options = {"ROCK","PAPER","SCISSORS"};
+            String ComputerOption = Options[random.nextInt((Options.length))];
+
+            System.out.println();
+
+            //Game Logic
+            if(PlayerChoice.equals("ROCK") && (ComputerOption.equals("SCISSORS"))){
                 System.out.println("YOU WIN !!");
-            }else{
-                System.out.println("YOU LOSE");
+            } else if (PlayerChoice.equals("PAPER") && (ComputerOption.equals("ROCK"))) {
+                System.out.println("YOU WIN !!");
+            } else if (PlayerChoice.equals("SCISSORS") && (ComputerOption.equals("PAPER"))) {
+                System.out.println("YOU WIN !!");
+            } else if (PlayerChoice.equals(ComputerOption)) {
+                System.out.println("ITS A DRAW !!");
+            }else if (!PlayerChoice.equals("ROCK") && !PlayerChoice.equals("PAPER") && !PlayerChoice.equals("SCISSORS")){
+                System.out.println("PLEASE ENTER A VALID CHOICE !! ");
+                continue;
+            }
+            else{
+                System.out.println("YOU LOSE :( ");
+            }
+            System.out.print("Opponent Choice: " + ComputerOption);
+
+            //Continue Option
+            System.out.print("PLAY AGAIN?(Press Y/N): ");
+            String contiOption = scanner.nextLine().toUpperCase();
+            if(contiOption.equals("N"))
+            {
+                System.out.println("---------------------------------");
+                System.out.println("THANK YOU FOR PLAYING WITH ME !! ");
+                System.out.println("---------------------------------");
+                Continue = false;
             }
 
-            System.out.print("Do u want to play again? (yes/no): ");
-            String playAgain = scanner.nextLine().toUpperCase();
-            if(playAgain.equals("NO"))
-            {
-                restart = false;
-            }
+
         }
 
 
-    }
-
-
-
-
+        scanner.close();
 
     }
+}
