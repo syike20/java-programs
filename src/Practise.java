4772e90a7d1657200c7da698f034c3e7f4beea6d
syike20 <dashrathrao28@gmail.com>
2026-07-03 23:53:50 +0530

    Added Java Program

---
+++ src/Practise.java
@@ -0,0 +1,8 @@
+public class Practise{
+    public static void main(String[] args ){
+        String[] topMovies = {"John Wick","Obsession","The Dark Knight"};
+        System.out.println(topMovies[0]);
+        topMovies[1] = "Parasite" ;
+        System.out.println(topMovies[1]);
+    }
+}
